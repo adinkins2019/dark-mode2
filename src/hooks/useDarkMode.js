e7dd3b6a9fe7ce7802952a088ee7ab95fae8b2ce
Antonio Dinkins <afdinki@gmail.com>
2020-02-22 23:40:15 -0500

    modified useDarkMode to add conditional statement

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -8,10 +8,10 @@ const useDarkMode = (props) => {
         //if DarkMode is turned on, then add dark-mode class to body element
         //if darkMode is turned off, then remove class from body element
         if (darkMode){
-
+            document.querySelector("body").classList.add("dark-mode");
         }
         else {
-
+            document.querySelector("body").classList.remove("dark-mode");
         };
     })
 
